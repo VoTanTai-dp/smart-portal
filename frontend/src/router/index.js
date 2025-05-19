ef9b089684705b8c41ed28429ff069a559bbeff9
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-19 10:57:58 +0700

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Navigator_bar from '@/components/layouts/navigation_bar.vue'
 import Home_page from '@/views/home_page.vue'
 import Account from '@/views/account_page.vue'
-// import Device from '@/views/device_page.vue'
-// import Report from '@/views/report_page.vue'
+import Device from '@/views/device_page.vue'
+import Report from '@/views/report_page.vue'
 
 const routes = [
   {
@@ -20,16 +20,16 @@ const routes = [
         name: 'account',
         component: Account
       },
-      // {
-      //   path: 'Devices',
-      //   name: 'devices',
-      //   component: Device
-      // },
-      // {
-      //   path: 'Reports',
-      //   name: 'reports',
-      //   component: Report
-      // }
+      {
+        path: 'Devices',
+        name: 'devices',
+        component: Device
+      },
+      {
+        path: 'Reports',
+        name: 'reports',
+        component: Report
+      }
     ]
   }
 ]
