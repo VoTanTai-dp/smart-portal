b05ec2052f38954e8099b9a082268b8778b4791d
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-19 09:34:12 +0700

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Navigator_bar from '@/components/layouts/navigation_bar.vue'
 import Home_page from '@/views/home_page.vue'
+import Account from '@/views/account_page.vue'
+// import Device from '@/views/device_page.vue'
+// import Report from '@/views/report_page.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: Navigator_bar,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: Home_page
+      },
+      {
+        path: 'Account',
+        name: 'account',
+        component: Account
+      },
+      // {
+      //   path: 'Devices',
+      //   name: 'devices',
+      //   component: Device
+      // },
+      // {
+      //   path: 'Reports',
+      //   name: 'reports',
+      //   component: Report
+      // }
+    ]
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home_page,
-    },
-    
-  ],
+  routes
 })
 
 export default router
