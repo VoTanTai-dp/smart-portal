eef55fe676f7ebd97cf0e4129430da97781b1a87
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-21 11:01:37 +0700

    Update navigationBar.vue

--- frontend/src/layouts/navigationBar.vue
+++ frontend/src/layouts/navigationBar.vue
@@ -3,31 +3,21 @@
 </script>
 
 <template>
-    <div class="full-width-container">
-        <div class="row">
-            <!-- Sidebar -->
-            <div class="col-1 sidebar">
-                <nav>
-                    <RouterLink
-                        style="text-decoration: none; color: white; display:flex; justify-content: center; align-items: center;"
-                        class="nav" to="/">Home</RouterLink>
-                    <RouterLink
-                        style="text-decoration: none; color: white; display:flex; justify-content: center; align-items: center;"
-                        class="nav" to="/Account">Account</RouterLink>
-                    <RouterLink
-                        style="text-decoration: none; color: white; display:flex; justify-content: center; align-items: center;"
-                        class="nav" to="/Devices">Device</RouterLink>
-                    <RouterLink
-                        style="text-decoration: none; color: white; display:flex; justify-content: center; align-items: center;"
-                        class="nav" to="/Reports">Reports</RouterLink>
-                </nav>
-            </div>
-
-            <!-- Main content -->
-            <div class="col-sm col-11 content">
-                <RouterView />
-            </div>
-        </div>
+    <div class="col-1 sidebar">
+        <nav>
+            <RouterLink
+                style="text-decoration: none; color: white; display:flex; justify-content: center; align-items: center;"
+                class="nav" to="/">Home</RouterLink>
+            <RouterLink
+                style="text-decoration: none; color: white; display:flex; justify-content: center; align-items: center;"
+                class="nav" to="/Account">Account</RouterLink>
+            <RouterLink
+                style="text-decoration: none; color: white; display:flex; justify-content: center; align-items: center;"
+                class="nav" to="/Devices">Device</RouterLink>
+            <RouterLink
+                style="text-decoration: none; color: white; display:flex; justify-content: center; align-items: center;"
+                class="nav" to="/Reports">Reports</RouterLink>
+        </nav>
     </div>
 </template>
 
