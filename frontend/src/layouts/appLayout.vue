b37899093e199471327b54b8ab4e626eb2178efa
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-21 10:52:49 +0700

    rename files

---
+++ frontend/src/layouts/appLayout.vue
@@ -0,0 +1,16 @@
+Import
+<script setup>
+import NavigationBar from './navigationBar.vue';
+
+</script>
+
+<template>
+    <div class="layout-wrapper">
+        <navigation-bar></navigation-bar>
+    </div>
+    <div class="layout-main-container">
+        <div class="layout-main">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
