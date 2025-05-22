e26ef542afbc920a97c3767cf2a151614698ec50
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-22 10:52:40 +0700

    download font awesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { all } from '@awesome.me/kit-KIT_CODE/icons'
+
+/* add icons to the library */
+library.add(...all)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
