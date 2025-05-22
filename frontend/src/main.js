09792333f8f1e150574ccbd2c408ba72b868a1e2
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-22 11:25:15 +0700

    import libs font awesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,22 +5,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// /* import the fontawesome core */
-// import { library } from '@fortawesome/fontawesome-svg-core'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-// /* import font awesome icon component */
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-// /* import specific icons */
-// import { all } from '@awesome.me/kit-KIT_CODE/icons'
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
-// /* add icons to the library */
-// library.add(...all)
+/* add icons to the library */
+library.add(faUserSecret)
 
 const app = createApp(App)
 
-// app.component('font-awesome-icon', FontAwesomeIcon)
-
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
