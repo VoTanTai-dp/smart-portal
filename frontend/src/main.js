68fbc5ca9cd6dd97d5917220c43edba30220df2c
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-23 08:03:48 +0700

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,8 +14,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
 /* add icons to the library */
 library.add(faUserSecret)
+
+library.add(faUserCircle)
 
 const app = createApp(App)
 
