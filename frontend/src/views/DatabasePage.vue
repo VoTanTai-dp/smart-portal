b411a3835337c39a7999fa07dee6c9acadb39c2a
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-24 11:20:18 +0700

    Create DatabasePage.vue

---
+++ frontend/src/views/DatabasePage.vue
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<template>
+    <div class="col-sm table-user">
+        <h2>table user</h2>
+    </div>
+
+    <div class="table-device">
+        <h2>table devices</h2>
+    </div>
+
+
+</template>
+
+<style scoped>
+.table-user{
+    background-color: var(--bg-secondary-color);
+}
+</style>
