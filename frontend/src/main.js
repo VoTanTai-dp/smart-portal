36223dcaf560bb2a896991d03633523eacba5857
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-24 08:06:50 +0700

    install & import bootstrap icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 import 'primeicons/primeicons.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
